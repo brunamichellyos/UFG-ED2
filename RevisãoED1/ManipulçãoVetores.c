3b8e7ef248fabb23a5e87954d5cc5cb641166ef8
Bruna Michelly de O. Silva <167580695+brunamichellyos@users.noreply.github.com>
2024-08-28 22:27:26 -0300

    Create ManipulçãoVetores.c

---
+++ RevisãoED1/ManipulçãoVetores.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int i, temp;
+
+    for (i = 0; i < 5; i++) {
+        temp = vetor[i];
+        vetor[i] = vetor[9 - i];
+        vetor[9 - i] = temp;
+    }
+
+    printf("Vetor invertido:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d ", vetor[i]);
+    }
+
+    return 0;
+}
