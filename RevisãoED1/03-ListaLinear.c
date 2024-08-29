a58e4610407cca3a2f5eacf7a90a95684ce66a74
Bruna Michelly de O. Silva <167580695+brunamichellyos@users.noreply.github.com>
2024-08-28 23:08:25 -0300

    Update 03-ListaLinear.c

--- RevisãoED1/03-ListaLinear.c
+++ RevisãoED1/03-ListaLinear.c
@@ -1,5 +1,14 @@
+/*
+O código abaixo apresenta a implementação de uma lista linear.
+
+Para esse exemplo, faça:
+- Implemente funções adicionais como buscar(int elemento) para encontrar a posição de um elemento na lista.
+ou 
+- Adicione métodos para inverter a lista, encontrar o máximo/mínimo, ou ordenar a lista.
+*/
+
 #include <stdio.h>
-#define MAX_SIZE 100
+#define MAX_SIZE 10
 
 int lista[MAX_SIZE];
 int tamanho = 0;
