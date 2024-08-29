80ed61efbcce315b31a68104dbff295cd99c6d90
Bruna Michelly de O. Silva <167580695+brunamichellyos@users.noreply.github.com>
2024-08-28 22:54:29 -0300

    Create 02-Matrizes.c

---
+++ RevisãoED1/02-Matrizes.c
@@ -0,0 +1,24 @@
+/*
+O código abaixo recebe do usuário elementos que são inseridos em uma matriz 3x3, em seguida, imprime a soma de sua diagonal principal.
+- faça uma modificação para que seja possível dinamizar o tamanho da matriz (utilizar alocação dinâmica de memória usando malloc)
+*/
+
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3];
+    int i, j, soma = 0;
+
+    printf("Insira os elementos da matriz 3x3:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matriz[i][j]);
+            if (i == j) {
+                soma += matriz[i][j];
+            }
+        }
+    }
+
+    printf("Soma dos elementos da diagonal principal: %d\n", soma);
+    return 0;
+}
