7fa25f330929426608a93442d3e6a29ab25a805e
Bruna Michelly de O. Silva <167580695+brunamichellyos@users.noreply.github.com>
2024-08-28 22:29:19 -0300

    Create ListaLinear.c

---
+++ RevisãoED1/ListaLinear.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int lista[MAX_SIZE];
+int tamanho = 0;
+
+void inserir(int elemento) {
+    if (tamanho < MAX_SIZE) {
+        lista[tamanho++] = elemento;
+    } else {
+        printf("Lista cheia!\n");
+    }
+}
+
+void remover(int index) {
+    if (index < 0 || index >= tamanho) {
+        printf("Índice inválido!\n");
+    } else {
+        for (int i = index; i < tamanho - 1; i++) {
+            lista[i] = lista[i + 1];
+        }
+        tamanho--;
+    }
+}
+
+void imprimir() {
+    printf("Elementos da lista:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", lista[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    inserir(10);
+    inserir(20);
+    inserir(30);
+    imprimir();
+    remover(1);
+    imprimir();
+    return 0;
+}
